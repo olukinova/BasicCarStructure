781808fdc9c385e3842b72ac6c5191b9041d8600
Olena Lukinova <95899106+olukinova@users.noreply.github.com>
2023-03-29 18:18:14 -0500

    The basic structure of the car is ready

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BasicStructure audi = new BasicStructure();
+        audi.color = "Black";
+        audi.model = "A5";
+        audi.power = "500cc";
+
+    }
+}
