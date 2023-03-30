022cd88f38d09917f3f99feb8139cf8ea985cff1
Olena Lukinova <95899106+olukinova@users.noreply.github.com>
2023-03-29 19:13:58 -0500

    The engine has been added

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,11 @@ public class Main {
         audi.model = "A5";
         audi.power = "500cc";
 
+
+        Engine x = new Engine();
+        x.cylinders = "8";
+        x.size = "1234cc";
+        x.weight = "6435k";
+        
     }
 }
